770333d471f4b4dd35b516199429b450cea64027
glacasse <glacasse@9bf9a32d-c66d-a80a-8625-f13047d698b2>
2014-09-12 15:37:54 +0000

    déplacé MovieDBContext

---
+++ trunk/AspDotNetTest/DBContexts/MovieDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using AspDotNetTest.Models;
+
+namespace AspDotNetTest.DBContexts
+{
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
