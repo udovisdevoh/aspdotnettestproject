5be5fa130c964e5005510ef6ee4586e0dc6be5d0
glacasse <glacasse@9bf9a32d-c66d-a80a-8625-f13047d698b2>
2014-09-12 15:37:54 +0000

    déplacé MovieDBContext

--- AspDotNetTest/Models/Movie.cs
+++ AspDotNetTest/Models/Movie.cs
@@ -2,21 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Data.Entity;
 
 namespace AspDotNetTest.Models
 {
     public class Movie
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
-    }
-
-    public class MovieDBContext : DbContext
-    {
-        public DbSet<Movie> Movies { get; set; }
     }
 }
