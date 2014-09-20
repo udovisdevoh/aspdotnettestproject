b3bf9bca7459b1cfa82c41dba4d0a786402e1d52
glacasse <glacasse@9bf9a32d-c66d-a80a-8625-f13047d698b2>
2014-09-20 21:18:04 +0000

    jquery globalize

---
+++ todo.txt.js
@@ -0,0 +1,58 @@
+Tech
+{
+	C#
+	{
+		Apprendre Asp.net
+		{	
+			MVC
+			{
+				http://www.asp.net/mvc/tutorials/mvc-5/introduction/getting-started
+				http://www.asp.net/mvc/tutorials/getting-started-with-ef-using-mvc/implementing-basic-crud-functionality-with-the-entity-framework-in-asp-net-mvc-application
+				http://azure.microsoft.com/en-us/documentation/articles/web-sites-dotnet-deploy-aspnet-mvc-app-membership-oauth-sql-database/?rnd=1
+				http://www.asp.net/aspnet/overview/developing-apps-with-windows-azure
+				
+				apprendre à faire des belongsTo, hasMany et hasAndBelongsToMany
+				
+				Faire site multilingue
+				{
+					http://msdn.microsoft.com/en-us/library/c6zyy3s9.aspx
+					http://www.c-sharpcorner.com/uploadfile/ankithakur/globalization_localization_in_dotnet_csharp07032006023510am/globalization_localization_in_dotnet_csharp.aspx
+				}
+			
+				Apprendre à utiliser mot de passe encrypté / apprendre à utiliser authentification par default
+				
+				Apprendre gestion de droits
+				
+				Validations: faire des messages d'erreur custom
+				
+				DB
+				{
+					Trouver moyen d'éditer la collation de table pour avoir un Where qui est case insensitive
+				
+					Créer index
+					{
+						unqiue
+						index à 2 clé
+					}
+				}
+				
+				custom code generation
+			}
+
+			.NET scaffolding model (from DB to model)
+			{
+				http://www.asp.net/visual-studio/overview/2013/aspnet-scaffolding-overview
+			}
+		}
+		
+		Apprendre WCF
+		
+		Réviser Linq
+		Apprendre pLinq
+		Apprendre à utiliser Team Foundation Server
+	}
+
+	Réviser C++
+	Réviser regex
+	Apprendre les bases de Perl
+}
